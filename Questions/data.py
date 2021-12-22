03893fd6a8aae94a6cb0f9b12a355a0544114d5a
Lawrence Y <84044148+lawrenceyim@users.noreply.github.com>
2021-12-21 19:02:54 -0500

    Added Quiz tutorial project

---
+++ Questions/data.py
@@ -0,0 +1,6 @@
+question_data = [
+    {"text": "The earth revolves around the sun.", "answer": "True"},
+    {"text": "The earth is flat.", "answer": "False"},
+    {"text": "The sun is a star.", "answer": "True"},
+    {"text": "Elon Musk sent a Tesla into space.", "answer": "True"}
+]
